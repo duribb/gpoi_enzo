61aa2a9ba34e5d7840d828c6f2eb438ee821e99e
lorenzorossoo <lollo.rouge@gmail.com>
2023-02-02 13:03:19 +0100

    Fine MOCK elaboratore di ricerca

--- mockElaboratoreDiRicerca.php
+++ mockElaboratoreDiRicerca.php
@@ -5,11 +5,11 @@
 
 
     if($tag>0){
-        $query = 'SELECT nomeFile, tag".PHP_EOL."FROM file".PHP_EOL."WHERE'; 
+        $query = 'SELECT nomeFile, tag'.PHP_EOL.'FROM file'.PHP_EOL.'WHERE '; 
         //In caso ci siano più tag aggiunge dopo il where le condizioni
-        $query = $query.'tag = '.'\"'.$tag[0].'\"';
+        $query = $query.'tag = '.'"'.$tag[0].'"';
         for($i=1; $i<$nTag; $i++){
-            $query = $query.' AND  tag = '.'\"'.$tag[$i].'\"';
+            $query = $query.' AND  tag = '.'"'.$tag[$i].'"';
         }
     }else{
         $query = 'SELECT nomeFile, tag'.PHP_EOL.'FROM file';
