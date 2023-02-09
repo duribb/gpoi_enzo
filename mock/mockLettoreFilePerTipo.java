338ee4b1a141d264c79ec781fe07dd3aabe1be88
lorenzorossoo <lollo.rouge@gmail.com>
2023-02-09 13:32:13 +0100

    Update mockLettoreFilePerTipo.java

---
+++ mock/mockLettoreFilePerTipo.java
@@ -0,0 +1,7 @@
+class mockLettoreFilePerTipo {
+    public static void main (String[] args){
+        /*Prendere in input un file qualsiasi, leggerlo e metterlo
+        all'interno di un file txt per facilitarne la lettura alle
+        altre features */
+    }
+}
