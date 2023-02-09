87cc9b8e37e6b9ba659e9872f9eb485812c97321
lorenzorossoo <lollo.rouge@gmail.com>
2023-02-09 12:54:42 +0100

    update elaboratoreDiRicerca.php

--- files/elaboratoreDiRicerca.php
+++ files/elaboratoreDiRicerca.php
@@ -1,11 +1,16 @@
 <?php
+    function trovaTag($tag){
+        for ($i=0; $i < count($tag); $i++) { 
+            $pos = strpos($tag[$i], "=");
+            $pos++; 
+            $tag[$i] = substr($tag[$i], -(strlen($tag[$i])-$pos));     
+        }
+        return $tag;   
+    }
+
     $var=$_SERVER['QUERY_STRING'];
 
     $tag = explode("&", $var);
 
-    print_r($tag);
-
-    function trovaTag(){
-
-    }
+    $tag = trovaTag($tag);
 ?>
