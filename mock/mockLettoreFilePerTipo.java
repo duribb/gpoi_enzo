3ebe30d3c91d5dc5251f26a989979887f4438ee9
lorenzorossoo <lollo.rouge@gmail.com>
2023-02-16 12:33:35 +0100

    Fine MockLetturaFilePerTipo
    Prende in input un file txt e stampa in console riga per riga il contenuto del file

--- mock/mockLettoreFilePerTipo.java
+++ mock/mockLettoreFilePerTipo.java
@@ -1,7 +1,26 @@
-class mockLettoreFilePerTipo {
-    public static void main (String[] args){
-        /*Prendere in input un file qualsiasi, leggerlo e metterlo
-        all'interno di un file txt per facilitarne la lettura alle
-        altre features */
+/*
+ * Per un corretto funzionamento del MOCK inserire un file di testo con estensione .txt
+ * Dopo l'apertura il MOCK provvederà a stampare a video riga per riga sulla console
+ */
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class mockLettoreFilePerTipo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("prova.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Errore apertura file");
+        }
+
+        String line = reader.readLine();
+        while (line != null) {
+            System.out.println(line);
+            line = reader.readLine();
+        }
     }
 }
