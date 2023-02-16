051b470893a97a8b0aa23ad26dd0429ff7520e22
lorenzorossoo <lollo.rouge@gmail.com>
2023-02-16 13:30:28 +0100

    trova estensione file in input

--- files/lettoreFilePerTipo.java
+++ files/lettoreFilePerTipo.java
@@ -4,16 +4,25 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class lettoreFilePerTipo {
-    public void letturaFile(String[] allPath) { //riceve come parametro un array di path, caso minimo: 1 path
+    public void letturaFile(String[] allPath) {
         for (int i=0; i<allPath.length; i++){
             BufferedReader reader = null;
             try {
-                reader = new BufferedReader(new FileReader()); //Inserire path cartella da analizzare
+                reader = new BufferedReader(new FileReader(allPath[i])); //Inserire path cartella da analizzare
             } catch (FileNotFoundException e) {
                 System.out.println("Errore apertura file");
                 System.exit(-1);
             }
         }
 
+    }
+
+    private String trovaEstensione(String path){
+        //Riceve in input un path, restituisce l'estensione
+        int len = path.length();
+        while (path.charAt(len-1) != '.'){
+            len--;
+        }
+        return path.substring(len);
     }
 }
