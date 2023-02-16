78bfc2dec03597d4e635a15efddd12822834797c
Alessandro Durigon <taitto.dury@gmail.com>
2023-02-16 13:31:48 +0100

    Create scriptCreazioneDB.sql

---
+++ files/scriptCreazioneDB.sql
@@ -0,0 +1,22 @@
+--script creazione database
+
+CREATE DATABASE TAGDB
+
+CREATE TABLE FILES(
+	nomefile VARCHAR(40), 
+	percorso VARCHAR(200),
+	PRIMARY KEY(nomefile)
+);
+
+CREATE TABLE ENZO(
+	nomefile VARCHAR(40),
+	tag VARCHAR(20),
+	PRIMARY KEY(nomefile, tag),
+	FOREIGN KEY(nomefile) REFERENCES FILES(NOMEFILE),
+	FOREIGN KEY(tag) REFERENCES TAGS(TAG)
+);
+
+CREATE TABLE TAGS(
+	tag VARCHAR(20),
+	PRIMARY KEY(TAG)
+);
