c7186009fa1804b5fb53d89c06df92cc7f849240
lorenzorossoo <lollo.rouge@gmail.com>
2023-02-16 13:00:19 +0100

    Update mockLettoreFilePerTipo
    -import.io.*
    -commenti per spiegare FileReader

--- mock/mockLettoreFilePerTipo.java
+++ mock/mockLettoreFilePerTipo.java
@@ -3,24 +3,27 @@
  * Dopo l'apertura il MOCK provvederà a stampare a video riga per riga sulla console
  */
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+ import java.io.*;
 
-public class mockLettoreFilePerTipo {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader("prova.txt"));
-        } catch (FileNotFoundException e) {
-            System.out.println("Errore apertura file");
-        }
-
-        String line = reader.readLine();
-        while (line != null) {
-            System.out.println(line);
-            line = reader.readLine();
-        }
-    }
-}
+ public class mockLettoreFilePerTipo {
+     public static void main(String[] args) throws IOException {
+         BufferedReader reader = null;
+         try {
+             reader = new BufferedReader(new FileReader("prova.txt"));
+             /*
+                 si può anche mettere il path all'interno del FileReader, in modo da leggere i file fuori
+                 dalla cartella del progetto, ma  all'interno delle cartelle alla quale abbiamo accesso
+             */
+         } catch (FileNotFoundException e) {
+             System.out.println("Errore apertura file");
+         }
+ 
+         String line = reader.readLine();
+         while (line != null) {
+             System.out.println(line);
+             line = reader.readLine();
+         }
+     }
+ }
+ 
+ 
