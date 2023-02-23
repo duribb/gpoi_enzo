12da0a222381640e9a6d9f196c0ffa7ba02b30ec
Lorenzo Rosso <lollo.rouge@gmail.com>
2023-02-23 13:27:21 +0100

    Update Lettore
    Aggiunta FOR

--- files/lettore/Lettore.java
+++ files/lettore/Lettore.java
@@ -1,11 +1,14 @@
 class Lettore {
-    BufferedReader lett(){
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader(allPath[i])); //Inserire path cartella da analizzare
-        } catch (FileNotFoundException e) {
-            System.out.println("Errore apertura file");
-            System.exit(-1);
+    public BufferedReader reader
+    Lettore(String[] allPath){
+        reader = null;
+        for(int i=0; i<allPath.length; i++){
+            try {
+                reader = new BufferedReader(new FileReader(allPath[i])); //Inserire path cartella da analizzare
+            } catch (FileNotFoundException e) {
+                System.out.println("Errore apertura file");
+                System.exit(-1);
+            }
         }
     }
 }
