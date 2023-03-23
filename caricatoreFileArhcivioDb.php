33ae5a2df6c9751f791d6c0423e84300e8677f28
Andreamariotti04 <75015143+Andreamariotti04@users.noreply.github.com>
2023-03-23 12:41:01 +0100

    Update caricatoreFileArhcivioDb.php

--- caricatoreFileArhcivioDb.php
+++ caricatoreFileArhcivioDb.php
@@ -2,8 +2,20 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   if (!empty($_FILES['elencoFiles'])) {
     $file_array = $_FILES['elencoFiles'];
-    $upload_dir = 'files_dir/'; // directory dove caricare i file
+    $upload_dir = 'archivio/waiting'; // directory dove caricare i file
     $file_count = count($file_array['name']);
+    
+
+
+    $nomehost = "localhost";
+    $nomeuser = "enzo";
+    $password = "enzo";
+    $database = "TAGDB";
+    $connessione = new mysqli($nomehost, $nomeuser, $password, $database);
+
+    if($connessione === false){
+      die("Errore nella connessione al database: " . $connessione->connect_error);
+    }
 
     for ($i = 0; $i < $file_count; $i++) {
       $file_name = $file_array['name'][$i];
@@ -15,10 +27,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
       if (move_uploaded_file($file_tmp, $upload_path)) {
         echo "Il file $file_name è stato caricato con successo.";
+        $sql = "INSERT INTO files (nomefile, percorso) VALUES ($file_name, $upload_path)";
+        if($connessione->query($sql) === true){
+          echo"caricamento file su db avvenuto con successo";
+        }else{
+          echo"errore nel caricamento del file sul db";
+        }
       } else {
         echo "Si è verificato un errore durante il caricamento del file.";
       }
     }
+    $connessione->clolse();
   }
 }
 ?>
