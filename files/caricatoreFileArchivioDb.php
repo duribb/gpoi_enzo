9f4609440f1aead86456e43d24a839b6d439a94c
Andreamariotti04 <75015143+Andreamariotti04@users.noreply.github.com>
2023-04-03 15:20:51 +0200

    Update caricatoreFileArchivioDb.php

--- files/caricatoreFileArchivioDb.php
+++ files/caricatoreFileArchivioDb.php
@@ -7,14 +7,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
 
 
-    $nomehost = "localhost";
-    $nomeuser = "enzo";
-    $password = "enzo";
-    $database = "TAGDB";
+    $nomehost = 'localhost';
+    $nomeuser = 'enzo';
+    $password = 'enzo';
+    $database = 'TAGDB';
     $connessione = new mysqli($nomehost, $nomeuser, $password, $database);
 
     if($connessione === false){
-      die("Errore nella connessione al database: " . $connessione);
+      die('Errore nella connessione al database: ' . $connessione);
     }
 
     for ($i = 0; $i < $file_count; $i++) {
@@ -26,15 +26,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       $upload_path = $upload_dir . basename($file_name);
 
       if (move_uploaded_file($file_tmp, $upload_path)) {
-        echo "Il file $file_name è stato caricato con successo.";
-        $sql = "INSERT INTO files (nomefile, percorso) VALUES ($file_name, $upload_path)";
+        echo 'Il file $file_name è stato caricato con successo.';
+        $sql = 'INSERT INTO files (nomefile, percorso) VALUES ($file_name, $upload_path)';
         if($connessione->query($sql) === true){
-          echo"caricamento file su db avvenuto con successo";
+          echo 'caricamento file su db avvenuto con successo';
         }else{
-          echo"errore nel caricamento del file sul db";
+          echo 'errore nel caricamento del file sul db';
         }
       } else {
-        echo "Si è verificato un errore durante il caricamento del file.";
+        echo 'Si è verificato un errore durante il caricamento del file.';
       }
     }
     $connessione->close();
