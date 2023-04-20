c9ac09795fda00aa6d4e1e5caf5e7e2765f676f0
Alessandro Durigon <taitto.dury@gmail.com>
2023-04-20 13:22:06 +0200

    post verifica 02

--- files/caricatoreFileArchivioDb.php
+++ files/caricatoreFileArchivioDb.php
@@ -2,10 +2,9 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   if (!empty($_FILES['elencoFiles'])) {
     $file_array = $_FILES['elencoFiles'];
-    $upload_dir = 'archivio/waiting'; // directory dove caricare i file
+    $upload_dir = 'archivio/waiting/'; // directory dove caricare i file
     $file_count = count($file_array['name']);
     
-
 
     $nomehost = 'localhost';
     $nomeuser = 'enzo';
@@ -26,8 +25,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       $upload_path = $upload_dir . basename($file_name);
 
       if (move_uploaded_file($file_tmp, $upload_path)) {
-        echo 'Il file $file_name è stato caricato con successo.';
-        $sql = 'INSERT INTO files (nomefile, percorso) VALUES ($file_name, $upload_path)';
+        echo 'Il file '.$file_name.' è stato caricato con successo.';
+        $sql = 'INSERT INTO files (nomefile, percorso) VALUES (\''.$file_name.'\',\''.$upload_dir.'\')';
         if($connessione->query($sql) === true){
           echo 'caricamento file su db avvenuto con successo';
         }else{
