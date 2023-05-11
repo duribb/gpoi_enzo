0c4402a41ea57860538b0fd9a2c7a373ea086513
levronz28 <levronetommaso@gmail.com>
2023-05-11 13:17:12 +0200

    Create interfaccia di Ricerca

---
+++ files/interfacciaRicercaTramiteFiltri.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>Ricerca tramite filtri</title>
+	</head>
+	<body>
+		<p>Selezionare i tag desiderati (Per selezionare più tag premere il tasto SHIFT):</p>
+		<form action="elaboratoreDiRicerca.php" method="get">
+			<select multiple size="4" name="listaTag">
+				<?php include 'connessione.php' ?>
+				<?php
+					$sql="SELECT tag FROM TAGS";
+
+					$res=$connessione->query($sql);
+
+					while($r=$res->fetch_assoc()){
+						echo "<option value=\"".$r["tag"]."\">".$r["tag"]."</option></br>";
+					}
+				?>
+			</select>
+			<input type="submit" value="Ricerca...">
+		</form>
+	</body>
+</html>
